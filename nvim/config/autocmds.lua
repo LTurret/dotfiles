72270953415ec1e7f96da17e1743405dc64f4456
LTurret <a0919329@gmail.com>
2023-11-26 19:32:35 +0800

    disable auto format

--- nvim/config/autocmds.lua
+++ nvim/config/autocmds.lua
@@ -4,24 +4,31 @@
 
 -- Turn off paster mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*",
-    command = "set nopaste",
-  })
-  
-  -- Fix conceallevel for json files
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "json", "jsonc" },
-    callback = function()
-      vim.wo.spell = false
-      vim.wo.conceallevel = 0
-    end,
-  })
-  
-  -- Black formatter
-  local group = vim.api.nvim_create_augroup("Black", { clear = true })
-  vim.api.nvim_create_autocmd("bufWritePost", {
-    pattern = "*.py",
-    command = "silent !black % -l 160",
-    group = group,
-  })
-  
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Disable autoformat for python
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "python" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
+
+-- Black formatter
+local group = vim.api.nvim_create_augroup("Black", { clear = true })
+vim.api.nvim_create_autocmd("bufWritePost", {
+  pattern = "*.py",
+  command = "silent !black % -l 160",
+  group = group,
+})
